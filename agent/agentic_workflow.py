3b29752df17a26f1b5018a75012e365c6f763ccf
kau-z <kaushalyaf34@gmail.com>
2026-02-22 16:50:11 +0530

    update agentic_workflow.py

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,28 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+#from tools.weather_info_tool import WeatherInfoTool
+#from tools.search_place_tool import SearchPlaceTool
+#from tools.calculator_tool import CalculatorTool
+#from tools.currency_conversion_tool import CurrencyConversionTool
+
+
+class GraphBuilder:
+    def __init__(self):
+        pass
+
+
+    def agent_function(self):
+        pass
+
+
+    def build_graph(self):
+        pass
+
+
+    def __call__(self):
+        pass
+
